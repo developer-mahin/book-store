c00228365a6fabba2d338ff807641fd6e55c7a21
developer-mahin <mdmahin1310@gmail.com>
2024-10-17 01:05:42 +0600

    completed to create function add to wishlisth

---
+++ js/script.js
@@ -0,0 +1,132 @@
+/* This function is for Hamburger menu */
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('show');
+});
+
+// navigate to home
+const logo = document.getElementById("logo")
+logo.addEventListener("click", () => {
+    document.location = "../home.html"
+})
+
+/* Load All book data */
+const loadBookData = () => {
+
+    fetch("https://gutendex.com/books")
+        .then((res) => res.json())
+        .then(data => displayBookData(data.results))
+        .catch(error => console.log(error))
+    spinner(true)
+}
+
+
+/* wishlist Option */
+let storeSingleBook = {}
+
+/* Display Book Data */
+const displayBookData = (books) => {
+
+    const bookListContainer = document.getElementById("book-list")
+    const bookRow = document.createElement("div")
+    bookRow.classList.add("book-row")
+
+    books.forEach(book => {
+        const { authors, bookshelves, copyright, formats, id, subjects, title } = book
+
+
+        storeSingleBook[id] = book
+
+        const bookDiv = document.createElement("div")
+        bookDiv.classList.add("book")
+
+        bookDiv.innerHTML = `
+        <div>
+            <img src="${formats["image/jpeg"]}" alt="${title}" />
+            <p class="author">${authors[0]?.name}</p>
+            <h3 class="title">${title.slice(0, 50) + "..."}</h3>
+
+        <div class="overlay">
+            <p class="overlay-btn wishlist-btn" data-book-id="${book.id}" id="wishlist"> 
+              <i class="fa-regular fa-heart"></i>
+            </p>
+            <p class="overlay-btn" >
+              <i class="fa-solid fa-cart-shopping"></i>
+            </p>
+            <p class="overlay-btn">
+              <i class="fa-solid fa-eye"></i>
+            </p>
+        </div>
+        </div>
+        `
+
+        bookRow.appendChild(bookDiv)
+        bookListContainer.appendChild(bookRow)
+
+    });
+
+
+    spinner(false)
+
+}
+
+
+// Spinner 
+const spinner = (isLoaded) => {
+    const spinnerContainer = document.getElementById('spinner-container')
+    if (isLoaded) {
+        spinnerContainer.style.cssText = 'display:flex'
+    }
+    else {
+        spinnerContainer.style.cssText = "display:none"
+    }
+}
+
+
+
+const bookList = document.getElementById("book-list")
+bookList.addEventListener("click", (event) => {
+    if (event.target.closest('.wishlist-btn')) {
+        const wishlistButton = event.target.closest('.wishlist-btn');
+        const bookId = wishlistButton.getAttribute('data-book-id');
+
+
+        const book = storeSingleBook[bookId]
+        addToWishlist(book);
+    }
+});
+
+const getFromLocalStorage = (storeName) => {
+    const data = localStorage.getItem(storeName)
+    return data ? JSON.parse(data) : [];
+}
+
+const setIntoLocalStorage = (storeName, data) => {
+
+    return localStorage.setItem(storeName, JSON.stringify(data))
+
+}
+
+const addToWishlist = (book) => {
+    let bookForAddInLocalStorage = getFromLocalStorage("wishlist") || [];
+
+    const isExistProduct = bookForAddInLocalStorage.find(
+        (item) => item.id === book.id
+    );
+
+    if (!isExistProduct) {
+        bookForAddInLocalStorage.push(book)
+        setIntoLocalStorage("wishlist", bookForAddInLocalStorage);
+        alert(`${book.title} has been added to the wishlist!`);
+    } else {
+        alert(`Book is already exist`);
+    }
+
+};
+
+
+
+
+loadBookData()
--- script.js
+++
@@ -1,7 +0,0 @@
-/* This function is for Hamburger menu */
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-});
